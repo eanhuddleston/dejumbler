01a107f2fe9dbbb0b9af7b0c9218d6ea1a7ae050
Ean Huddleston <ean.huddleston@gmail.com>
2013-06-11 21:54:29 -0700

    Clean up code a bit

--- dejumble.rb
+++ dejumble.rb
@@ -1,15 +1,16 @@
 class Dejumbler
+  attr_reader :dictionary
+
   def initialize
-    @corpus = read_corpus
-    @tree = build_trie_tree(@corpus)
+    @dictionary = read_dictionary
+    @tree = build_trie_tree(dictionary)
   end
 
   def dejumble(input)
-    all_distinct_combos = all_distinct_letter_combos(input)
-    new_real_words = find_new_real_words(all_distinct_combos, input)
+    all_distinct_letter_combos = all_distinct_letter_combos(input)
+    all_distinct_letter_combos.delete(input)
     
-    puts new_real_words
-    new_real_words
+    identify_real_words(all_distinct_letter_combos)
   end
 
   def all_distinct_letter_combos(input)
@@ -26,12 +27,11 @@ class Dejumbler
 
           unless ( word_base.count(next_letter) >=
               input_letters.count(next_letter) ) ||
-              all_combos.include?(possible_word)
+              all_combos.include?(possible_word) ||
+              !in_tree?(possible_word)
 
-            if in_tree?(possible_word)
-              all_combos << possible_word
-              next_round << possible_word
-            end
+            all_combos << possible_word
+            next_round << possible_word
           end
         end
       end
@@ -56,58 +56,60 @@ class Dejumbler
     true
   end
 
-  def find_new_real_words(to_check, input)
-    new_real_words = []
-    corpus = read_corpus
+  def identify_real_words(to_check)
+    real_words = []
     to_check.each do |possible_word|
-      if possible_word != input &&
-          corpus_includes?(possible_word, corpus)
-            
-        new_real_words << possible_word
+      if dictionary_includes?(possible_word)    
+        real_words << possible_word
       end
     end
 
-    new_real_words.sort
+    real_words.sort
   end
 
-  def corpus_includes?(possible_word, corpus) 
-    while true
-      middle = (corpus.length - 1) / 2
+  def dictionary_includes?(possible_word)
+    return true if binary_search(dictionary, possible_word)
 
-      if corpus[middle] == possible_word
-        return true
-      elsif possible_word < corpus[middle]
-        return false if middle == 0
-        corpus = corpus[0...middle]
-      elsif possible_word > corpus[middle]
-        return false if middle == corpus.length - 1
-        corpus = corpus[middle+1..corpus.length-1]
-      end
+    false
+  end
+
+  def binary_search(array, value, from = 0, to = nil)
+    to ||= array.count - 1
+    return nil if from > to
+ 
+    middle = (from + to) / 2
+ 
+    if value < array[middle]
+      binary_search(array, value, from, middle - 1)
+    elsif value > array[middle]
+      binary_search(array, value, middle + 1, to)
+    else
+      middle
     end
   end
 
-  def read_corpus
-    corpus = []
+  def read_dictionary
+    dictionary = []
     File.foreach('dictionary.txt') do |line|
-      corpus << line.chomp
+      dictionary << line.chomp
     end
 
-    corpus
+    dictionary
   end
 
-  def build_trie_tree(corpus)
-    top_node = TreeNode.new(nil, nil)
-    corpus = read_corpus
-    corpus.each do |word|
+  def build_trie_tree(dictionary)
+    top_node = TrieTreeNode.new(nil)
+    dictionary = read_dictionary
+    dictionary.each do |word|
       node = top_node
       word.each_char do |char|
         match = find_child(node, char)
         if match.empty?
-          new_node = TreeNode.new(node, char)
+          new_node = TrieTreeNode.new(char)
           node.children << new_node
           node = new_node
         else
-          node = match[0] # move to that node
+          node = match[0]
         end
       end
     end
@@ -120,16 +122,15 @@ class Dejumbler
   end
 end
 
-class TreeNode
+class TrieTreeNode
   attr_accessor :children
   attr_reader :value
 
-  def initialize(parent, value)
-    @parent = parent
+  def initialize(value)
     @value = value
     @children = []
   end
 end
 
-Dejumbler.new.dejumble("phantasmagoria")
+puts Dejumbler.new.dejumble("phantasmagoria")
 
